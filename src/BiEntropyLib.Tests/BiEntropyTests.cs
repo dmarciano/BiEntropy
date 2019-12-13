a8604110080a7d21979ce4c0c0f3f93e60e2d95e
Dominick Marciano <dominickmarciano@gmail.com>
2019-12-13 15:12:35 -0500

    Initial code commit

---
+++ src/BiEntropyLib.Tests/BiEntropyTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BiEntropyLib.Tests
+{
+    [TestClass]
+    public class BiEntropyTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ src/BiEntropyLib.Tests/TBiEntropyTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BiEntropyLib.Tests
+{
+    [TestClass]
+    public class TBiEntropyTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
